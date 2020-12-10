ac1753bf638780fc40496caeb46237f7c09b0166
renaissanceMan92 <timholm.contact@gmail.com>
2020-12-11 00:20:26 +0100

    Förbättrat sökrutan. Lagt till mkt basic filmlista.

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -17,25 +17,21 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: AppColors.primaryColor,
+          backgroundColor: AppColors.secondaryColor,
           title: Text('The Movies App', textAlign: TextAlign.center),
           actions: <Widget>[
             PopupMenuButton<int>(
               icon: Icon(Icons.more_vert_rounded, color: Colors.white),
               itemBuilder: (context) => [
-                PopupMenuItem(
-                  value: 1,
-                  child: Text('Första'),
-                ),
+                PopupMenuItem(value: 1,  child: Text('Första'),),
                 PopupMenuItem(value: 2, child: Text('Andra'))
               ],
-              onSelected: (value) {
-                print("value:$value");
-              },
+              onSelected: (value) => print("value:$value"),
             )
           ],
         ),
         body: _movieWidget(context, String));
+    // Tror vi kan ta bort grejerna under här.
     /*Center(
         child: Container( 
             decoration: BoxDecoration(
@@ -51,18 +47,14 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 Widget _movieWidget(context, title) {
-  TextEditingController textEditingController;
   return Card(
       child: Container(
-          child: TextField(
-            controller: textEditingController,
-            decoration: InputDecoration(
-              labelText: 'search for movies',
-              labelStyle:
-                  TextStyle(color: Colors.black87, fontWeight: FontWeight.w300),
-            ),
-          ),
           decoration: BoxDecoration(
+            gradient: LinearGradient( // Flyttade lineargradient hit.
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [AppColors.primaryColor, AppColors.secondaryColor],
+            ),
             color: AppColors.secondaryColor,
             borderRadius: BorderRadius.only(
               bottomLeft: Radius.circular(10),
@@ -70,5 +62,65 @@ Widget _movieWidget(context, title) {
               topRight: Radius.circular(10),
               bottomRight: Radius.circular(10),
             ),
-          )));
+          ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget> [
+              _searchBar(),
+              _movieList(),
+            ]
+          )
+        )
+      )
+  );
+}
+
+// Sökrutan.
+Widget _searchBar() {
+  TextEditingController textEditingController;
+  return Row(
+      children: <Widget> [
+        Expanded(
+          child: TextField(
+            controller: textEditingController,
+            decoration: InputDecoration(
+              labelText: 'Search for movies',
+              labelStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.w400),
+            ),
+          ),
+        ),
+        IconButton(
+            icon: Icon(Icons.search),
+            color: Colors.white,
+            onPressed: () {
+              // Utför sökning, gör någonting med textEditingController.
+            })
+      ]
+  );
+}
+
+// Listan med filmerna.
+Widget _movieList(){
+  return Expanded(
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView.builder(
+        itemCount: 6, // Visar 6 st filmer nu.
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget> [
+                // Här är filmerna.
+                Text("Avengers: Endgame", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400)),
+                Text("120 min", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400)),
+                Text("Rating: 4.5 stars", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400))
+              ]
+            )
+          );
+        }
+      ),
+    ),
+  );
 }

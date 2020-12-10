f06f04b2e9c985d8d0b39339d2cb85b777fd0f6f
kuojinNerverland <gusjhakjca@student.gu.se>
2020-12-10 19:32:42 +0100

    Widget för filmerna

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:MoviePKR/util/constants.dart';
 import 'package:flutter/material.dart';
 
@@ -14,11 +16,12 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
           backgroundColor: AppColors.primaryColor,
           title: Text('The Movies App', textAlign: TextAlign.center),
           actions: <Widget>[
             PopupMenuButton<int>(
+              icon: Icon(Icons.more_vert_rounded, color: Colors.white),
               itemBuilder: (context) => [
                 PopupMenuItem(
                   value: 1,
@@ -26,17 +29,38 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 PopupMenuItem(value: 2, child: Text('Andra'))
               ],
+              onSelected: (value) {
+                print("value:$value");
+              },
             )
-          ]),
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  color: [AppColors.primaryColor, AppColors.secondaryColor])),
+          ],
         ),
+        body: _movieWidget(context, String));
+    /*Center(
+        child: Container( 
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            color: [AppColors.primaryColor, AppColors.secondaryColor],
+          )),
+            ),
       ),
-    );
+    );*/
   }
+}
+
+Widget _movieWidget(context, title) {
+  return Card(
+      margin: const EdgeInsets.all(1.0),
+      child: Container(
+          decoration: BoxDecoration(
+        color: AppColors.secondaryColor,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(15),
+          topLeft: Radius.circular(15),
+          topRight: Radius.circular(15),
+          bottomRight: Radius.circular(15),
+        ),
+      )));
 }

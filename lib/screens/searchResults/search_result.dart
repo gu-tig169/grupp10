31b8c5461caace38008f142b1d9fc14600c399f7
renaissanceMan92 <timholm.contact@gmail.com>
2020-12-28 13:10:30 +0100

    Fetching searched movies is working. Displaying the movies needs more work (pictures cannot be added yet, and the gridview setup is only temporary).

--- lib/screens/searchResults/search_result.dart
+++ lib/screens/searchResults/search_result.dart
@@ -1,8 +1,11 @@
-import 'package:MoviePKR/widgets/movieList_listview.dart';
 import 'package:flutter/material.dart';
 import 'package:MoviePKR/util/constants.dart';
 import 'package:MoviePKR/screens/main_page.dart';
 import 'package:flutter/rendering.dart';
+import 'package:MoviePKR/providers/movieLists_provider.dart';
+import 'package:MoviePKR/models/Movie.dart';
+
+import '../movieDescription_screen.dart';
 
 class SearchResult extends StatefulWidget {
 
@@ -14,8 +17,17 @@ class SearchResult extends StatefulWidget {
 }
 
 class _SearchResultState extends State<SearchResult> {
+
+  List<Movie> movieList;
+  Future<List<Movie>> getMovieList(String query) async {
+    movieList = await MovieLists.fetchMovies(query);
+    return movieList;
+  }
+
   @override
   Widget build(BuildContext context) {
+    getMovieList(widget.search);
+    //var searchedList = Provider.of<MovieLists>(context).trendingList;
     return Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
@@ -35,22 +47,92 @@ class _SearchResultState extends State<SearchResult> {
             ),
             centerTitle: true,
           ),
-          body: Column(children: <Widget>[
-            // Padding here caused keyboard gap.
-            searchBar(context),
-            Padding(
-              padding: EdgeInsets.fromLTRB(8, 16, 8, 8),
-              child: Container(
-                  child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(widget.search,
+          body: _aWidget(),
+        ));
+
+
+
+  }
+
+  Widget _aWidget() {
+    return FutureBuilder<List<Movie>>(
+        future: getMovieList(widget.search),
+        // ignore: missing_return
+        builder: (context, AsyncSnapshot<List<Movie>> snapshot) {
+          if (snapshot.hasData) {
+            return Column(children: <Widget>[
+              // Padding here caused keyboard gap.
+              searchBar(context),
+              Padding(
+                padding: EdgeInsets.fromLTRB(8, 16, 8, 8),
+                child: Container(
+                    child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text("Showing results for: '" + widget.search + "'",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500)))),
+              ),
+              _movieList(context),
+            ]);
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        }
+    );
+  }
+
+
+  Widget _movieList(BuildContext context) {
+
+    var size = MediaQuery.of(context).size;
+    final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
+    final double itemWidth = size.width / 2;
+
+    return Expanded(
+      child: GridView.count(
+        crossAxisCount: 2,
+        childAspectRatio: (itemWidth / itemHeight),
+        children: List.generate(movieList.length, (index) {
+          return InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) =>
+                      new DescriptionScreen(id: movieList[index].id)));
+            },
+            child: Card(
+              shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+              elevation: 1,
+              color: Colors.transparent,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(movieList[index].title,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
                           style: TextStyle(
                               color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500)))),
-            ),
-            Expanded(child: MovieListWidget()),
-          ]),
-        ));
+                              fontSize: 18,
+                              fontWeight: FontWeight.w800)),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 5),
+                    child: Text(movieList[index].rating.toString(),
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.w600)),),],),),);}),),);
+
   }
+
+
+
 }

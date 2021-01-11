22d4daec1318ec15cdfd7888f2b14905e27f0377
Tarek Alzein <tfzein@gmail.com>
2021-01-11 10:54:33 +0100

    Added try catch in splash screen to check API connection

--- lib/screens/splashScreen/splash_screen.dart
+++ lib/screens/splashScreen/splash_screen.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:MoviePKR/providers/movieLists_provider.dart';
 import 'package:MoviePKR/screens/mainPage/main_page.dart';
+import 'package:MoviePKR/screens/savedLists/savedLists_screen.dart';
 import 'package:MoviePKR/util/constants.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:splashscreen/splashscreen.dart';
 
@@ -11,17 +15,52 @@ class MainSplashScreen extends StatefulWidget {
 }
 
 class _MainSplashScreenState extends State<MainSplashScreen> {
-
   Future<Widget> loadFromFuture() async {
-    await Provider.of<MovieLists>(context, listen: false).fetchTrendingList();
-    await Provider.of<MovieLists>(context, listen: false).fetchGenres();
+    try {
+      await Provider.of<MovieLists>(context, listen: false)
+          .fetchTrendingList()
+          .timeout(Duration(minutes: 1));
+      await Provider.of<MovieLists>(context, listen: false)
+          .fetchGenres()
+          .timeout(Duration(minutes: 1));
+    } on SocketException catch (_) {
+      // return Future.value(SavedList());
+
+      return showDialog(
+        context: context,
+        barrierDismissible: false, // user must tap button!
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Connection interrupted'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  Text('Please check your internet connection then try again.'),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text('Exit'),
+                onPressed: () {
+                  exit(0);
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } on TimeoutException catch (_) {
+      print('timedout');
+    } on Error catch (e) {
+      print('Error: $e');
+    }
     try {
       await Provider.of<MovieLists>(context, listen: false)
           .fetchSavedListsFromSF();
     } catch (error) {
-      throw error;
+      print('Error in Shared preferences data-fetching $error');
     }
-
     return Future.value(new MyHomePage(title: 'MoviePKR2'));
   }
 
